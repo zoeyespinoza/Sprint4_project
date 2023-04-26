5c1630404e31a0cc03cdff2ce161a19fcdfb9c73
zoeyvero <z.vero.visual@gmail.com>
2023-04-26 15:31:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
 import plotly.express as px
 import pandas as pd
 import streamlit as st
+train = pd.read_csv(
+    '/Users/zoeyespinoza/Documents/GitHub/Sprint4_project/train.csv')
+test = pd.read_csv('/Users/zoeyespinoza/Documents/GitHub/Sprint4_project/test.csv')
+
+titanic_df = pd.merge(train, test, on='PassengerId', how='outer')
+titanic_df.to_csv('merged.csv', index=False)
+
+st.header("Titanic Passenger Data")
+color_by_survival = st.checkbox("Color by Survival")
+
+fig_age = px.histogram(titanic_df, x="Age_x", nbins=20,
+                       color="Survived",
+                       title="Distribution of Passenger Age")
+
+
+fig_fare = px.histogram(titanic_df, x="Fare_x", nbins=20, 
+                        color="Survived",
+                        title="Distribution of Passenger Fares")
+
+fig_scatter = px.scatter(titanic_df, x="Age_x", y="Fare_x", 
+                         color="Survived",
+                         title="Passenger Age vs. Fare")
+
+
+# Show the figures in the notebook
+fig_age.show()
+fig_fare.show()
+fig_scatter.show()
 
